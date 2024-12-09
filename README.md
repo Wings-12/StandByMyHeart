66ffbd1934e2f568cb471b37c5d326342b180e46
WingOmi <t.omi@sonicmoov.com>
2024-12-10 01:26:55 +0900

    フレームワークをNext.jsに変更

--- README.md
+++
@@ -1,3 +0,0 @@
-# StandByMyHeart
-
-[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/Wings-12/StandByMyHeart)
--- src/components/JournalEntry.tsx
+++
@@ -1,5 +0,0 @@
-import { JournalForm } from './JournalForm';
-
-export const JournalEntry: React.FC = () => {
-  return <JournalForm className="w-full max-w-md mx-auto" />;
-};
